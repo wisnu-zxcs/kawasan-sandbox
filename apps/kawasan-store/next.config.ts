5877b5cd4e3be4e6da95ec139f4f1c879e531adb
Wisnu Putra Jiwangga <128898605+wisnu-zxcs@users.noreply.github.com>
2026-05-16 20:34:28 +0900

    repo: refactor and reconfiguring repository patterns

--- apps/kawasan-store/next.config.ts
+++ apps/kawasan-store/next.config.ts
@@ -3,7 +3,10 @@ import { type NextConfig } from "next"
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  transpilePackages: ["@repo/ui"]
+  transpilePackages: [
+    "@repo/server",
+    "@repo/ui"
+  ]
 }
 
 export default nextConfig
